a983c39e445d594b025525577883314ede933338
Matej Ajster <ma6468@student.uni-lj.si>
2018-09-06 15:18:56 +0200

    Implemented redux with same functionality

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,15 +1,30 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
 import './App.css';
+
+import { setSearchField } from '../actions';
+
+const mapStateToProps = (state) => {
+	return {
+		searchField: state.searchField,
+	}
+}
+
+
+const mapDispatchToProps = (dispatch) => {
+	return {
+		onSearchChange: (event) => dispatch(setSearchField(event.target.value))
+	}
+}
 
 class App extends Component {
 	constructor() {
 		super();
 		this.state = {
 			robots: [],
-			searchfield: '',
 		}
 	}
 
@@ -19,29 +34,25 @@ class App extends Component {
 		.then(users => this.setState({ robots: users}))
 	}
 
-	onSearchChange = (event) => {
-		this.setState({ searchfield: event.target.value });
-	}
 	render() {
-		const filteredRobots = this.state.robots.filter(robot => {
-			return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+		const { robots } = this.state;
+		const { searchField, onSearchChange } = this.props
+		const filteredRobots = robots.filter(robot => {
+			return robot.name.toLowerCase().includes(searchField.toLowerCase());
 		});
 
-		if (this.state.robots.length === 0) {
-			return <h1 className='tc'>Loading... Please wait</h1>
-		} else {
-			return (
-				<div className='tc'>
-					<h1 className='f1' >Robofriends</h1>
-					<SearchBox searchChange={ this.onSearchChange }/>
-					<Scroll>
-						<CardList robots={ filteredRobots } />
-					</Scroll>
-				</div>
-			);
-		}
+		return !robots.length === 0 ?
+			<h1 className='tc'>Loading... Please wait</h1> :
+			<div className='tc'>
+				<h1 className='f1' >Robofriends</h1>
+				<SearchBox searchChange={ onSearchChange }/>
+				<Scroll>
+					<CardList robots={ filteredRobots } />
+				</Scroll>
+			</div>
+			
 	}
 	
 }
 
-export default App;
+export default connect(mapStateToProps, mapDispatchToProps)(App);
